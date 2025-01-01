be81fa6eab758922116b34d550bad8847e88161a
Abinesh-Srinivasan <abineshsrinivasan1301@gmail.com>
2025-01-01 16:58:16 +0530

    Read button Popup card design completed

--- app/components/ExpenseDisplay.tsx
+++ app/components/ExpenseDisplay.tsx
@@ -1,26 +1,82 @@
-import {
-  View,
-  Text,
-  FlatList,
-  Button,
-  TouchableOpacity,
-  Image,
-} from "react-native";
+import { useState } from "react";
+import { View, Text, FlatList, TouchableOpacity, Image } from "react-native";
 
 const editIcon = require("@/assets/images/ExpenseCardIcons/edit.png");
 const deleteIcon = require("@/assets/images/ExpenseCardIcons/delete.png");
 
 const ExpenseDisplay = () => {
+  const [showPopup, setShowPopup] = useState(false);
+  const [popupItem, setPopupItem] = useState<{
+    category: string;
+    amount: number;
+    date: string;
+    id: number;
+    description: string;
+  } | null>(null);
+
   const exampleExpenses = [
-    { id: 1, category: "Food", amount: 150, date: "2024-12-30" },
-    { id: 2, category: "Travel", amount: 500, date: "2024-12-28" },
-    { id: 3, category: "Shopping", amount: 200, date: "2024-12-25" },
-    { id: 4, category: "Shopping", amount: 200, date: "2024-12-25" },
-    { id: 5, category: "Shopping", amount: 200, date: "2024-12-25" },
-    { id: 6, category: "Shopping", amount: 200, date: "2024-12-25" },
-    { id: 7, category: "Shopping", amount: 200, date: "2024-12-25" },
-    { id: 8, category: "Shopping", amount: 200, date: "2024-12-25" },
-    { id: 9, category: "Shopping", amount: 200, date: "2024-12-25" },
+    {
+      id: 0,
+      category: "Food",
+      amount: 150,
+      date: "2024-12-30",
+      description: "Hi, this is Nesharo",
+    },
+    {
+      id: 2,
+      category: "Travel",
+      amount: 500,
+      date: "2024-12-28",
+      description: "Hi, this is Nesharo",
+    },
+    {
+      id: 3,
+      category: "Shopping",
+      amount: 200,
+      date: "2024-12-25",
+      description:
+        "Hi, this is Nesharo, hello, how are youshflsh, farith suresh ganesh raja diljaz abhimanyu, Hi, this is Nesharo, hello, how are youshflsh, Hi, this is Nesharo",
+    },
+    {
+      id: 5,
+      category: "Shopping",
+      amount: 200,
+      date: "2024-12-25",
+      description:
+        "Hi, this is Nesharo, hello, how are youshflsh, farith suresh ganesh raja diljaz abhimanyu, Hi, this is Nesharo, hello, how are youshflsh, Hi, this is Nesharo",
+    },
+    {
+      id: 4,
+      category: "Shopping",
+      amount: 200,
+      date: "2024-12-25",
+      description:
+        "Hi, this is Nesharo, hello, how are youshflsh, farith suresh ganesh raja diljaz abhimanyu, Hi, this is Nesharo, hello, how are youshflsh, Hi, this is Nesharo",
+    },
+    {
+      id: 7,
+      category: "Shopping",
+      amount: 200,
+      date: "2024-12-25",
+      description:
+        "Hi, this is Nesharo, hello, how are youshflsh, farith suresh ganesh raja diljaz abhimanyu, Hi, this is Nesharo, hello, how are youshflsh, Hi, this is Nesharo",
+    },
+    {
+      id: 8,
+      category: "Shopping",
+      amount: 200,
+      date: "2024-12-25",
+      description:
+        "Hi, this is Nesharo, hello, how are youshflsh, farith suresh ganesh raja diljaz abhimanyu, Hi, this is Nesharo, hello, how are youshflsh, Hi, this is Nesharo",
+    },
+    {
+      id: 9,
+      category: "Shopping",
+      amount: 200,
+      date: "2024-12-25",
+      description:
+        "Hi, this is Nesharo, hello, how are youshflsh, farith suresh ganesh raja diljaz abhimanyu, Hi, this is Nesharo, hello, how are youshflsh, Hi, this is Nesharo",
+    },
   ];
 
   type Expense = {
@@ -28,13 +84,24 @@ const ExpenseDisplay = () => {
     category: string;
     amount: number;
     date: string;
+    description: string;
+  };
+
+  const handlePopupOpen = (item: Expense) => {
+    setShowPopup(true);
+    setPopupItem(item);
+  };
+
+  const handlePopupClose = () => {
+    setShowPopup(false);
+    setPopupItem(null);
   };
 
   const renderExpenseCard = ({ item }: { item: Expense }) => {
     let date = item.date;
     item.date = date.split("-").reverse().join("-");
     return (
-      <View className=" flex flex-row mt-5 border border-gray-300 rounded-2xl px-6 py-4 items-center justify-between">
+      <View className=" flex flex-row mt-5 border border-t-white border-l-white border-r-white border-b-gray-300 px-6 pb-4 items-center justify-between">
         {/* Category,Amount,Date */}
         <View className=" flex flex-col">
           <Text className=" font-rubik-bold text-2xl text-red-500 tracking-wide">
@@ -49,7 +116,10 @@ const ExpenseDisplay = () => {
         </View>
         {/* Buttons */}
         <View className=" flex flex-row items-center gap-3">
-          <TouchableOpacity className="border border-white px-3 py-1 rounded-lg bg-green-500">
+          <TouchableOpacity
+            className="border border-white px-3 py-1 rounded-lg bg-green-500"
+            onPress={() => handlePopupOpen(item)}
+          >
             <Text className=" font-rubik-regular tracking-wide text-white">
               Read
             </Text>
@@ -73,6 +143,32 @@ const ExpenseDisplay = () => {
         keyExtractor={(item) => item.id.toString()}
         renderItem={renderExpenseCard}
       />
+      {/* popup card */}
+      {showPopup && (
+        <View className=" absolute top-56 bottom-56 left-20 bg-white h-80 w-9/12 px-5 py-8 shadow-inherit shadow-lg flex flex-col justify-between rounded-2xl">
+          <View className=" flex flex-col gap-3">
+            <Text className=" font-rubik-bold text-pink-600">
+              {popupItem?.date}
+            </Text>
+            <View className=" flex  flex-row justify-between">
+              <Text className=" font-rubik-semibold text-2xl text-violet-600">
+                {popupItem?.category}
+              </Text>
+              <Text className=" font-rubik-semibold text-blue-600 text-lg">
+                ₹.{popupItem?.amount}
+              </Text>
+            </View>
+            <Text className=" font-rubik-regular leading-6 tracking-wide text-gray-700">
+              {popupItem?.description}
+            </Text>
+          </View>
+          <TouchableOpacity onPress={handlePopupClose}>
+            <Text className=" text-center font-rubik-semibold text-red-600 text-lg">
+              Close
+            </Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 };
